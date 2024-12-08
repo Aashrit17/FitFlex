c4528728daebeb6e615ab1fdac94ee6dbe2506e4
Aashrit17 <aashrit17038gmail.comgit config --global user.name Aashrit17>
2024-12-08 19:28:13 +0545

    initial commit

---
+++ lib/app.dart
@@ -0,0 +1,23 @@
+import 'package:fitflex/view/home_page.dart';
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialRoute: '/login',
+      routes: {
+        '/login': (context) => const LoginPage(),
+        '/home': (context) => const HomePage(),
+      },
+    );
+  }
+}
