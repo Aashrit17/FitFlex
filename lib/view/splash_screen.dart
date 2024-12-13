5c69de12d85e62dda1d0e13921599f865e2eb932
Aashrit17 <aashrit17038gmail.comgit config --global user.name Aashrit17>
2024-12-13 10:29:09 +0545

    splash screen

--- lib/view/splash_screen.dart
+++ lib/view/splash_screen.dart
@@ -1,1 +1,50 @@
+import 'dart:async';
 
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(const Duration(seconds: 3), () {
+      Navigator.pushReplacementNamed(context, '/login');
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              'assets/images/fitflex.png',
+              height: 150,
+            ),
+            const SizedBox(height: 20),
+            const Text(
+              "FitFlex",
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
+            ),
+            const SizedBox(height: 20),
+            CircularProgressIndicator(
+              color: Colors.purple[400],
+              strokeWidth: 3,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
