c69d736a5f0f7011264fe83db595c7ec0c1612b7
Aashrit17 <aashrit17038gmail.comgit config --global user.name Aashrit17>
2025-01-16 13:09:27 +0545

    additional architecture

---
+++ lib/features/auth/data/model/auth_hive_model.dart
@@ -0,0 +1,60 @@
+import 'package:equatable/equatable.dart';
+import 'package:fitflex/app/constants/hive_table_constants.dart';
+import 'package:fitflex/features/auth/domain/entity/auth_entity.dart';
+import 'package:hive_flutter/adapters.dart';
+import 'package:uuid/uuid.dart';
+
+@HiveType(typeId: HiveTableConstant.userTableId)
+class AuthHiveModel extends Equatable {
+  @HiveField(0)
+  final String? userId;
+  @HiveField(1)
+  final String name;
+  @HiveField(2)
+  final String email;
+  @HiveField(3)
+  final String phone;
+  @HiveField(4)
+  final String password;
+
+  AuthHiveModel({
+    String? userId,
+    required this.name,
+    required this.email,
+    required this.phone,
+    required this.password,
+  }) : userId = userId ?? const Uuid().v4();
+
+  // Initial Constructor
+  const AuthHiveModel.initial()
+      : userId = '',
+        name = '',
+        email = '',
+        phone = '',
+        password = '';
+
+  // From Entity
+  factory AuthHiveModel.fromEntity(AuthEntity entity) {
+    return AuthHiveModel(
+      userId: entity.userId,
+      name: entity.name,
+      email: entity.email,
+      phone: entity.phone,
+      password: entity.password,
+    );
+  }
+
+  // To Entity
+  AuthEntity toEntity() {
+    return AuthEntity(
+      userId: userId,
+      name: name,
+      email: email,
+      phone: phone,
+      password: password,
+    );
+  }
+
+  @override
+  List<Object?> get props => [userId, name, email, phone, password];
+}
